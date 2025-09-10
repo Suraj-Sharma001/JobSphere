140da722051a1d6112450d315e7d6c4c643df2b3
Suraj Sharma <surajsharma60923@gmail.com>
2025-09-10 16:43:06 +0530

    not

--- client/src/api/communityApi.js
+++ client/src/api/communityApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "https://job-sphere-zrdl.vercel.app/api"});
+const API = axios.create({ baseURL: "http://localhost:5000/api" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("userInfo")) {
